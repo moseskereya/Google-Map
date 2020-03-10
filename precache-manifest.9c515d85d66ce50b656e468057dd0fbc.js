55cf4f2894e85111809ccbfcba8f959c868520fa
Moses <lazaromoses77@gmail.com>
2020-03-10 15:37:58 +0300

    Updates

---
+++ precache-manifest.9c515d85d66ce50b656e468057dd0fbc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c22fd9f177ba1b4696f98c870882d51",
+    "url": "/Google-Map/index.html"
+  },
+  {
+    "revision": "cc9d66f02d2b675cf44b",
+    "url": "/Google-Map/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "043bef2ac302648382bb",
+    "url": "/Google-Map/static/js/2.49fae2ea.chunk.js"
+  },
+  {
+    "revision": "76337fd691e4155561d496e490beeac9",
+    "url": "/Google-Map/static/js/2.49fae2ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc9d66f02d2b675cf44b",
+    "url": "/Google-Map/static/js/main.2e1ddb72.chunk.js"
+  },
+  {
+    "revision": "03a44ab2f80b4c3b939d",
+    "url": "/Google-Map/static/js/runtime-main.5e4c0bf7.js"
+  }
+]);
